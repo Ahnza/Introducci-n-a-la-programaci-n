b5e118b0e7c8300c41ae31c472543b70356cdabe
Ahnza <charlyrivero1999@gmail.com>
2025-03-27 14:58:06 -0400

    Create practica 2(2) ejercicio15.cpp

---
+++ practica 2(2) ejercicio15.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 51;
+    int b = 876;
+    int c = 235;
+
+    
+    cout << "The values are: a = " << a << ", b = " << b << ", c = " << c << endl;
+
+    
+    cout << "The values are: a = " << b << ", b = " << c << ", c = " << a << endl;
+
+    return 0;
+}
