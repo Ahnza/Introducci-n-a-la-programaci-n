e7c167e2975d9469ba6828b287610c6a349b5d47
Ahnza <charlyrivero1999@gmail.com>
2025-03-27 14:09:28 -0400

    Create practica 2, ejercicio 3.cpp
    He terminado el ejercicio 3

---
+++ practica 2, ejercicio 3.cpp
@@ -0,0 +1,22 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int weidth = 0;
+    int height = 0;
+
+    
+    cout << "Please enter rectangle weidth: "<< endl;
+    cin >> weidth;
+    
+    cout << "Please enter rectangle height: "<< endl;
+    cin >> height;
+    
+    int area = weidth * height;
+    
+    
+    cout << "Rectangle area is = " << area << " m²." << endl;
+    return 0;
+}
