51dc3ca6381d7124c9c6619cc7a40b6bacff16a4
Ahnza <charlyrivero1999@gmail.com>
2025-03-27 14:10:32 -0400

    Create practica 2, ejercicio 4.cpp
    Finalizado el ejercicio 4

---
+++ practica 2, ejercicio 4.cpp
@@ -0,0 +1,25 @@
+
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std; 
+
+int main() {
+    
+    int x = 0;
+    int y = 0;
+    int z = 1;
+    
+    cout << "Please enter a integer number 'x': " << endl;
+    cin >> x;
+    
+    cout << "Please enter a just integer number for  'y': " << endl;
+    cin >> y;
+    
+    int result = x + y + z;
+    
+    
+    cout << "Your answer: " << result << endl;
+    
+   return 0;
+   
+}
