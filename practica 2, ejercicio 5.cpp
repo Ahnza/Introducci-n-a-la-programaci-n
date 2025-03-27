7a7f73be4ab3b77c04bdc64166bbac4713ea17a5
Ahnza <charlyrivero1999@gmail.com>
2025-03-27 14:11:00 -0400

    Create practica 2, ejercicio 5.cpp

---
+++ practica 2, ejercicio 5.cpp
@@ -0,0 +1,16 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std; 
+
+int main() {
+    
+  int x, y;
+  
+  //Para una mejor visión ponemos alguna orden.
+  cout << "Please enter a number for 'x' and 'y': " << endl;
+  cin >> x >> y;
+  cout << "Result = " << x + y;
+    
+   return 0;
+   
+}
