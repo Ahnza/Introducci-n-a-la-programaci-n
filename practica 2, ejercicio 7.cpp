6183a65912e0a1d1101ca8c7c651adb58e8e525d
Ahnza <charlyrivero1999@gmail.com>
2025-03-27 14:13:13 -0400

    Create practica 2, ejercicio 7.cpp

---
+++ practica 2, ejercicio 7.cpp
@@ -0,0 +1,22 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    
+    string title_1, title_2, title_3 ;
+    
+    cout << "Please enter the title books: " << endl;
+    getline(cin, title_1);
+    getline(cin, title_2);
+    getline(cin, title_3);
+    
+    cout << "\n";
+    
+    cout <<  title_3 << endl; 
+    cout <<  title_2 << endl; 
+    cout <<  title_1 << endl; 
+    
+    return 0;
+}
